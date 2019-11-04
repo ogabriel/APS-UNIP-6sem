db9bcd26a0bf7ffbfcbacf20c9c0461281b12859
Gabriel Oliveira <gabrieloliver8991@gmail.com>
2019-11-03 23:34:57 -0300

    basic fingerprint enhancement code (#2)
    * add frequency module
    
    * add ridge_frequency module
    
    * git add ridge_orientation module
    
    * add ridge_segmentation module
    
    * add ridge_filter module
    
    * add enhance_iamge_enhance module
    
    * add app.py
    
    * add copyright notice

---
+++ enhance/image_enhance.py
@@ -0,0 +1,38 @@
+from .ridge_segmentation import ridge_segment
+from .ridge_orientation import ridge_orient
+from .ridge_frequency import ridge_freq
+from .ridge_filter import ridge_filter
+
+
+def image_enhance(img):
+    blksze = 16
+    thresh = 0.1
+    # normalise the image and find a ROI
+    normim, mask = ridge_segment(img, blksze, thresh)
+
+    gradientsigma = 1
+    blocksigma = 7
+    orientsmoothsigma = 7
+    # find orientation of every pixel
+    orientim = ridge_orient(
+        normim,
+        gradientsigma,
+        blocksigma,
+        orientsmoothsigma)
+
+    blksze = 38
+    windsze = 5
+    minWaveLength = 5
+    maxWaveLength = 15
+    # find the overall frequency of ridges
+    freq, medfreq = ridge_freq(
+        normim, mask, orientim, blksze, windsze, minWaveLength, maxWaveLength)
+
+    freq = medfreq * mask
+    kx = 0.65
+    ky = 0.65
+    # create gabor filter and do the actual filtering
+    newim = ridge_filter(normim, orientim, freq, kx, ky)
+
+    #th, bin_im = cv2.threshold(np.uint8(newim),0,255,cv2.THRESH_BINARY);
+    return(newim < -3)
