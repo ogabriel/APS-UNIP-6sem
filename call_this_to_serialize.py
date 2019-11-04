55a25a414820518a236d149feadd336e172cbda7
Jessica Mitiko <jessicamitiko@hotmail.com>
2019-11-04 23:50:34 +0000

    fix the filename

--- call_this_to_serialize.py
+++ call_this_to_serialize.py
@@ -4,5 +4,5 @@ import app
 
 images_name = ['101_1.tif', '102_1.tif', '103_1.tif'] # achar um jeito de puxar isso automatico
 path = "database/permitted/"
-filename = "samples_processed"
+filename = "permitted_processed"
 app.serializes(images_name, path, filename)
